731759bb2cbe6009c902e334bd84d96c4add0b21
Kameron Ibraheem <kameronibraheem@gmail.com>
2024-12-02 19:16:47 -0600

    Create TestWeather.java

---
+++ TestWeather.java
@@ -0,0 +1,14 @@
+// TestWeather.java
+// Kameron Ibraheem
+// Lab 05: TestWeather
+//
+
+public class TestWeather {
+    public static void main(String[] args) {
+		
+        Weather weatherReport = new Weather("Minneapolis");
+
+        // Display the weather report
+        weatherReport.displayWeatherReport();
+    }
+}
